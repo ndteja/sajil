b092baa02d99b302c5e6c4c595b5199d19fde37f
teja <tejanakka1@gmail.com>
2018-01-12 18:38:48 +0530

    v.2

--- js/main.js
+++
@@ -1,9 +0,0 @@
-var btn = document.getElementsByClassName('btn');
-var sezPhase = document.getElementsById('sezPhase');
-
-btn.addEventListener('click', viewLink);
-
-function viewLink(){
-    sezPhase.style.display='block';
-    btn.style.display='none';
-}
